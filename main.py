141d821588a105c7bc5ad0fc968a5acea4032136
bsx2 <BsxHomu@outlook.com>
2023-08-16 16:30:53 +0800

    feat: order generate

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
 import json
+import inquirer
 import re
 import qrcode
 import os
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
-import time
 import base64
 from cv2 import imread
 from pyzbar.pyzbar import decode
 
-Config = None
-WebDriver = None
-
 def init():
     print("读取配置文件...")
     try:
-        with open('Config.json', 'r') as cfgFile:
+        with open('Config.json', 'r', encoding='utf-8') as cfgFile:
             try:
-                global Config  
-                Config = json.load(cfgFile)
+                global config  
+                config = json.load(cfgFile)
                 pattern = re.compile(r'(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]')
-                if not bool(pattern.match(Config.get("target"))):
+                if not bool(pattern.match(config.get("target"))):
                     print("请使用合法url")
                     exit()
-                if not Config["userinfo"]["name"] or not Config["userinfo"]["phone"]:
+                if not config["userinfo"]["name"] or not config["userinfo"]["phone"]:
                     print("姓名或手机号不能为空")
                     exit()
             except json.JSONDecodeError as e:
@@ -41,24 +40,22 @@ def init():
     chrome_options.add_argument('--ignore-certificate-errors')
     chrome_options.add_argument('--disable-notifications')
     chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    global WebDriver
-    WebDriver = webdriver.Chrome(options=chrome_options)
+    global web_driver
+    web_driver = webdriver.Chrome(options=chrome_options)
 
 def login():
     while(not try_login()):
         pass
 
 def try_login():
-    WebDriver.get(Config["target"])
-    WebDriver.find_element(By.CLASS_NAME, "nav-header-register").click()
-    time.sleep(1)
+    web_driver.get(config["target"])
+    web_driver.find_element(By.CLASS_NAME, "nav-header-register").click()
     get_QR()
-
-    input("扫码登录确认后，按enter继续...")
-    time.sleep(1)
+    print('请在1分钟内扫码确认')
     try:
-        WebDriver.find_element(By.CLASS_NAME, "profile-img")
-    except NoSuchElementException as e:
+        wait = WebDriverWait(web_driver, 60) 
+        wait.until(EC.presence_of_element_located((By.CLASS_NAME, "profile-img")))
+    except TimeoutException as e:
         print(f"登录失败：\n{e.msg}\n")
         input("按enter重试...")
         return False
@@ -68,8 +65,10 @@ def try_login():
 
 def get_QR():
     try:
-        base64str = WebDriver.find_element(By.XPATH, "//img[@alt='Scan me!']").get_attribute("src").split(",")[1]
-    except NoSuchElementException as e:
+        wait = WebDriverWait(web_driver, 10) 
+        s = wait.until(EC.presence_of_element_located((By.XPATH, "//img[@alt='Scan me!']")))
+        base64str = s.get_attribute("src").split(",")[1]
+    except TimeoutException as e:
         print(f"获取登录二维码失败:{e.msg}")
         exit()
     
@@ -83,8 +82,57 @@ def get_QR():
     qr.add_data(qrUrl)
     qr.print_ascii(invert=True)
 
+def select():
+    select_box = web_driver.find_element(By.CLASS_NAME, "login-show-wrapper")
+    l1 = select_box.find_elements(By.XPATH, "./ul[1]/li[2]/div")
+    l1 = [div for div in l1 if 'unable' not in div.get_attribute('class')]
+    sessions = [x.text for x in l1];
+    options = [inquirer.List('choice',
+                            message="选择场次",
+                            choices=sessions)]
+    answers = inquirer.prompt(options)['choice']
+    index = [index for index, value in enumerate(l1) if value.text == answers][0]
+    l1[index].click()
+    l2 = select_box.find_elements(By.XPATH, "./ul[2]/li[2]/div")
+    l2 = [div for div in l2 if 'unable' not in div.get_attribute('class')]
+    prices = [x.text for x in l2];
+    options = [inquirer.List('choice',
+                            message="选择价格",
+                            choices=prices)]
+    answers = inquirer.prompt(options)['choice']
+    index = [index for index, value in enumerate(l2) if value.text == answers][0]
+    l2[index].click()
+    num = input('输入购票数量：')
+    try:
+        num = int(num)
+        if (num <= 0):
+            print('请输入大于零的数')
+            exit()
+    except ValueError:
+        print("无效输入，请输入一个整数。")
+        exit()
+    plus = select_box.find_element(By.XPATH, ".//div[contains(@class, 'ticket-count')]/div[contains(@class, 'count-plus')]")
+    while num > 1:
+        plus.click()
+        num-=1
+    buy = select_box.find_element(By.XPATH, ".//div[contains(@class, 'product-buy-wrapper')]/div[1]/div[1]")
+    buy.click()
+    wait = WebDriverWait(web_driver, 10) 
+    wait.until(EC.presence_of_element_located((By.XPATH, "//section[contains(@class, 'contact-block')]")))
+    iss = web_driver.find_elements(By.XPATH, "//section[contains(@class, 'contact-block')]//input")
+    iss[0].clear()
+    iss[0].send_keys(config['userinfo']['name'])
+    iss[1].clear()
+    iss[1].send_keys(config['userinfo']['phone'])
+    c = web_driver.find_element(By.XPATH, "//div[contains(@class, 'service-term')]/span[1]")
+    if 'checked' not in c.get_attribute('class'):
+        c.click()
+    c = web_driver.find_element(By.XPATH, "//div[contains(@class, 'confirm-paybtn')]")
+    c.click()
+    print('订单已生成')
 
 
 if __name__ == '__main__':
     init()
     login()
+    select()
